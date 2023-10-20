27030e7a05919830d42d28418fd11b52a453fec1
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-20 22:33:31 +0530

    header, variables, global styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Header.scss'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header text-white'>
+      <div className='container'>
+        <div className='header-cnt'>
+          <div className='header-cnt-top fs-13 py-2 flex align-center justify-between'>
+            <div className='header-cnt-top-l'>
+              <ul className='flex top-links align-center'>
+                <li>
+                  <Link to='/seller'>Seller Center</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
