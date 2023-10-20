b53da936deed65c7d369be248fbbe39fb19211d6
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-20 19:25:39 +0530

    webpack, eslint, prettier setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+        <h1>Splurge</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin  = require("mini-css-extract-plugin");
+
+let mode = "development";
+let target = "web";
+
+if(process.env.NODE_ENV === "production"){
+    mode = "production";
+    target = "browserslist";
+}
+
+module.exports = {
+    mode: mode,
+    target: target,
+    entry: './src/main.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js',
+        assetModuleFilename: "assets/[name][ext][query]"
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: "./dist",
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options:{
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        })
+    ],
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+}
