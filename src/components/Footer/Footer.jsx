e450fbb31dd2e23e1bfc4e149c5722b714171c7b
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-28 11:07:50 +0530

    fixed footer, Cart page functionality

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import './Footer.scss'
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className='footer bg-yellow'>
-      <div className='container py-4 text-center'>
-        <div className='flex align-center justify-center text-white fw-3 fs-14'>
-          <Link to='/' className='text-uppercase'>Privacy Policy</Link>
-          <div className='vert-line'></div>
-          <Link to='/' className='text-uppercase'>terms of service</Link>
-          <div className='vert-line'></div>
-          <Link to='/' className='text-uppercase'>About Splurge</Link>
-        </div>
+      <div className='container py-2 text-center'>
+        
         <span className='text-white copyright-text text-manrope fs-14 fw-3'>&copy; {new Date().getFullYear()} Splurge | All Rights Reserved.</span>
       </div>
     </footer>
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './CartPage.scss'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -23,8 +23,103 @@ const Cart = () => {
       </div>
     )
   }
+  
   return (
-    <div>Cart</div>
+    <div className='cart bg-whitesmoke'>
+      <div className='container'>
+        <div className='cart-ctable'>
+          <div className='cart-chead bg-white'>
+            <div className='cart-ctr fw-6 font-manrope fs-15'>
+              <div className='cart-cth'>
+                <span className='cart-ctxt'>S.No</span>
+              </div>
+              <div className='cart-cth'>
+                <span className='cart-ctxt'>Product</span>
+              </div>
+              <div className='cart-cth'>
+                <span className='cart-ctxt'>Unit Price</span>
+              </div>
+              <div className='cart-cth'>
+                <span className='cart-ctxt'>Quantity</span>
+              </div>
+              <div className='cart-cth'>
+                <span className='cart-ctxt'>Total Price</span>
+              </div>
+              <div className='cart-cth'>
+                <span className='cart-ctxt'>Actions</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='cart-cbody bg-white'>
+          {
+            carts.map((cart, idx) => {
+              return (
+                <div className='cart-ctr py-4' key={cart?.id}>
+                  <div className='cart-ctd'>
+                    <span className='cart-ctxt'>{idx + 1}</span>
+                  </div>
+                  <div className='cart-ctd'>
+                    <span className='cart-ctxt'>{cart?.title}</span>
+                  </div>
+                  <div className='cart-ctd'>
+                    <span className='cart-ctxt'>{formatPrice(cart?.discountedPrice)}</span>
+                  </div>
+                  <div className='cart-ctd'>
+                    <div className='qty-change flex align-center'>
+                      <button type='button' className='qty-decrease flex align-center justify-center' onClick={() => dispatch(toggleCartQty({id: cart?.id, type: "DESC"}))}>
+                        <i className='fas fa-minus'></i>
+                      </button>
+
+                      <div className='qty-value flex align-center justify-center'>
+                      {cart?.quantity}
+                      </div>
+
+                      <button type='button' className='qty-increase flex align-center justify-center' onClick={() => dispatch(toggleCartQty({id: cart?.id, type: "INC"}))}>
+                        <i className='fas fa-plus'></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className='cart-ctd'>
+                    <span className='cart-ctxt text-yellow fw-5'>{formatPrice(cart?.totalPrice)}</span>
+                  </div>
+
+                  <div className='cart-ctd'>
+                    <button type='button' className='delete-btn text-dark' onClick={() => dispatch(removeFromCart(cart?.id))}>
+                    <i className="fa-solid fa-trash-can mx-1"></i> <span>Delete</span>
+                    </button>
+                  </div>
+
+                </div>
+              )
+            })
+          }
+          </div>
+
+          <div className='cart-cfoot flex align-start justify-between py-3 bg-white'>
+            <div className='cart-cfoot-l'>
+              <button type='button' className='clear-cart-btn text-danger fs-15 text-uppercase fw-4' onClick={() => dispatch(clearCart())}>
+                <i className="fa-solid fa-trash-can"></i>
+                <span className='mx-1'>Clear Cart</span>
+              </button>
+            </div>
+
+            <div className='cart-cfoot-r flex flex-column justify-end'>
+              <div className='total-txt flex align-center justify-end'>
+                <div className='font-manrope fw-5'>Total ({itemsCount}) items: </div>
+                <span className='text-yellow fs-22 mx-2 fw-6'>{formatPrice(totalAmount)}</span>
+              </div>
+
+              <button type='button' className='checkout-btn text-white bg-yellow fs-16'>Check Out</button>
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 
